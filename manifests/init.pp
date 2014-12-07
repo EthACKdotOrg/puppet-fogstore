dbf6aa1375ade2c1ea6b3cbe91544e6dd772db73
Cédric Jeanneret <tengu@ethack.org>
2014-12-07 13:36:02 +0100

    added doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,74 @@
+# == Class fogstore
+#
+# Entry point for xtreemfs/openssl/jks wrappers
+#
+# == Parameters
+# === Mandatory
+#
+# [*cred_password*]
+#       Password for credential container.
+#
+# [*role*]
+#       Node role. Accepted:
+#       client, dir, mrc, osd
+#
+# [*ssl_source_dir*]
+#       Source directory for SSL certificates.
+#       Might be either on the filesystel (file:///…)
+#       or a puppet resource (puppet:///modules/…)
+#
+# [*trusted_password*]
+#       Password for trusted container.
+#
+# === Optional
+#
+# [*add_repo*]
+#       Tell the module to add XtreemFS repository
+#       and related GPG key.
+#
+# [*apt_key*]
+#       In case you want to add your own repository, provide
+#       APT key.
+#
+# [*apt_key_src*]
+#       In case you want to add your own repository, provide
+#       its APT key source
+#
+# [*cred_format*]
+#       Credential container format.
+#
+# [*dir_service*]
+#       Directory service address
+#
+# [*manage_ssl*]
+#       Should fogstore manage your SSL certificate?
+#       If so, it will create the necessary credential and
+#       trusted containers.
+#
+# [*properties*]
+#       Xtreemfs configuration hash as listed in documentation:
+#       http://xtreemfs.org/xtfs-guide-1.5/index.html#tth_sEc3.2.6
+#
+# [*object_dir*]
+#       Xtreemfs object destination. Should be some RAID[156] volume
+#       for better security.
+#
+# [*release*]
+#       APT repository related "release" field
+#
+# [*repos*]
+#       APT repository path
+#
+# [*pkg_source*]
+#       Your own APT repository for XtreemFS packages
+#       Note: if you sed $add_repo to false and set
+#       pkg_source to some value, it *will* add your repository.
+#       Setting $add_repo to "true" and setting your own custom repository
+#       will only add your repository.
+#
+# [*trusted_format*]
+#       Trusted store container format.
+#
 class fogstore(
   $cred_password,
   $role,
@@ -17,6 +88,10 @@ class fogstore(
   $trusted_format = $fogstore::params::trusted_format,
 ) inherits fogstore::params {
 
+  if $role !~ /client|dir|mrc|osd/ {
+    fail "Fogstore: unknown node role: ${role}"
+  }
+
   if $pkg_source {
     $_repository = false
     ::apt::source {'xtreemfs':
@@ -31,6 +106,7 @@ class fogstore(
   } else {
     $_repository = $add_repository,
   }
+
   if $role ~ /osd|mrc|dir/ and $manage_ssl {
     file {$::fogstore::params::trust_location:
       ensure  => directory,
