34f82bfb61eabb92e0e4cb1a0bce5395858fe54c
Cédric Jeanneret <tengu@ethack.org>
2014-12-10 06:33:11 +0100

    added other JKS checks

--- spec/classes/ssl_trusted_spec.rb
+++ spec/classes/ssl_trusted_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe 'fogstore::ssl::trusted' do
   let(:params) {{
     :client_ca           => 'client-ca.pem',
-    :client_jks_password => 'fooBar',
     :dir_ca              => 'dir-ca.pem',
     :dir_jks_password    => 'barBaz',
     :mrc_ca              => 'mrc-ca.pem',
@@ -13,7 +12,7 @@ describe 'fogstore::ssl::trusted' do
     :ssl_source_dir      => '.',
   }}
 
-  describe "should create java_ks dir_client_ca" do
+  describe "should create java_ks for DIR" do
     it { 
       should contain_java_ks('dir_client_ca')
       .with('ensure'      => 'latest')
@@ -33,6 +32,36 @@ describe 'fogstore::ssl::trusted' do
       .with('certificate' => './osd-ca.pem')
       .with('target' => '/etc/xos/xtreemfs/truststore/dir.jks')
       .with('password' => 'barBaz')
+    }
+  end
+  describe "should create java_ks for MRC" do
+    it { 
+      should contain_java_ks('mrc_client_ca')
+      .with('ensure'      => 'latest')
+      .with('certificate' => './client-ca.pem')
+      .with('target'      => '/etc/xos/xtreemfs/truststore/mrc.jks')
+      .with('password'    => 'abcd')
+    }
+    it { 
+      should contain_java_ks('mrc_dir_ca:/etc/xos/xtreemfs/truststore/mrc.jks')
+      .with('ensure'      => 'latest')
+      .with('certificate' => './dir-ca.pem')
+      .with('password'    => 'abcd')
+    }
+  end
+  describe "should create java_ks for OSD" do
+    it { 
+      should contain_java_ks('osd_client_ca')
+      .with('ensure'      => 'latest')
+      .with('certificate' => './client-ca.pem')
+      .with('target'      => '/etc/xos/xtreemfs/truststore/osd.jks')
+      .with('password'    => 'efgh')
+    }
+    it { 
+      should contain_java_ks('osd_dir_ca:/etc/xos/xtreemfs/truststore/osd.jks')
+      .with('ensure'      => 'latest')
+      .with('certificate' => './dir-ca.pem')
+      .with('password'    => 'efgh')
     }
   end
 end
