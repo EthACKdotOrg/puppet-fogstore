433b350e0b6e08e76d6f6b7de3e9e4d91557fa21
Cédric Jeanneret <tengu@ethack.org>
2014-12-10 18:53:55 +0100

    using global facts

--- spec/classes/role_osd_spec.rb
+++ spec/classes/role_osd_spec.rb
@@ -1,48 +1,48 @@
 require 'spec_helper'
 
-describe 'fogstore::roles::osd' do
-  let(:params) {{
-    :add_repo         => 'false',
-    :cred_format      => 'pkcs12',
-    :cred_password    => 'barBaz',
-    :credential       => 'osd.p12',
-    :properties       => {},
-    :ssl_source_dir   => '.',
-    :trusted          => 'osd.jks',
-    :trusted_format   => 'jks',
-    :trusted_password => 'fooBar',
-  }}
+os_facts = @os_facts
 
-  let(:facts) {{
-    'lsbdistid'              => 'Debian',
-    'lsbdistcodename'        => 'jessie',
-    'operatingsystem'        => 'Debian',
-    'operatingsystemrelease' => '8.0',
-    'osfamily'               => 'Debian',
-  }}
+os_facts.each do |osfamily, facts|
+  describe 'fogstore::roles::osd' do
+    let(:params) {{
+      :add_repo         => 'false',
+      :cred_format      => 'pkcs12',
+      :cred_password    => 'barBaz',
+      :credential       => 'osd.p12',
+      :properties       => {},
+      :manage_jks       => false,
+      :ssl_source_dir   => '.',
+      :trusted          => 'osd.jks',
+      :trusted_format   => 'jks',
+      :trusted_password => 'fooBar',
+    }}
+    let :facts do
+      facts
+    end
 
 
-  describe "should contain xtreemfs::role::storage" do
-    it {
-      should contain_class('xtreemfs::role::storage')
-      .with('add_repo'                => 'false')
-      .with('dir_host'                => 'localhost')
-      .with('properties'              => {
-        "ssl.enabled"                 => true,
-        'ssl.service_creds'           => '/etc/ssl/certs/osd.p12',
-        'ssl.service_creds.container' => 'pkcs12',
-        'ssl.service_creds.pw'        => 'barBaz',
-        'ssl.trusted_certs'           => '/etc/xos/xtreemfs/truststore/osd.jks',
-        'ssl.trusted_certs.container' => 'jks',
-        'ssl.trusted_certs.pw'        => 'fooBar'
-      })
-    }
-  end
+    describe "should contain xtreemfs::role::storage" do
+      it {
+        should contain_class('xtreemfs::role::storage')
+        .with('add_repo'                => 'false')
+        .with('dir_host'                => 'localhost')
+        .with('properties'              => {
+          "ssl.enabled"                 => true,
+          'ssl.service_creds'           => '/etc/ssl/certs/osd.p12',
+          'ssl.service_creds.container' => 'pkcs12',
+          'ssl.service_creds.pw'        => 'barBaz',
+          'ssl.trusted_certs'           => '/etc/xos/xtreemfs/truststore/osd.jks',
+          'ssl.trusted_certs.container' => 'jks',
+          'ssl.trusted_certs.pw'        => 'fooBar'
+        })
+      }
+    end
 
-  describe "should push credential file" do
-    it {
-      should contain_file('/etc/ssl/certs/osd.p12')
-      .with('source' => './osd.p12')
-    }
+    describe "should push credential file" do
+      it {
+        should contain_file('/etc/ssl/certs/osd.p12')
+        .with('source' => './osd.p12')
+      }
+    end
   end
 end
