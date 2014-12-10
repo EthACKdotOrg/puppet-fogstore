999d36acd1b66592a4527af6ff060d729e1838c3
Cédric Jeanneret <tengu@ethack.org>
2014-12-10 18:10:53 +0100

    corrected/updated spec

--- spec/classes/role_osd_spec.rb
+++ spec/classes/role_osd_spec.rb
@@ -13,23 +13,36 @@ describe 'fogstore::roles::osd' do
     :trusted_password => 'fooBar',
   }}
 
+  let(:facts) {{
+    'lsbdistid'              => 'Debian',
+    'lsbdistcodename'        => 'jessie',
+    'operatingsystem'        => 'Debian',
+    'operatingsystemrelease' => '8.0',
+    'osfamily'               => 'Debian',
+  }}
+
+
   describe "should contain xtreemfs::role::storage" do
-    should contain_class('xtreemfs::role::storage')
-    .with('add_repo'                => 'false')
-    .with('dir_service'             => 'localhost')
-    .with('properties'              => {
-      'ssl.enable'                  => true,
-      'ssl.service_creds'           => '/etc/ssl/certs/osd.p12',
-      'ssl.service_creds.container' => 'pkcs12',
-      'ssl.service_creds.pw'        => 'barBaz',
-      'ssl.trusted_certs'           => '/etc/xos/xtreemfs/truststore/osd.jks',
-      'ssl.trusted_certs.container' => 'jks',
-      'ssl.trusted_certs.pw'        => 'fooBar',
-    })
+    it {
+      should contain_class('xtreemfs::role::storage')
+      .with('add_repo'                => 'false')
+      .with('dir_host'                => 'localhost')
+      .with('properties'              => {
+        "ssl.enabled"                 => true,
+        'ssl.service_creds'           => '/etc/ssl/certs/osd.p12',
+        'ssl.service_creds.container' => 'pkcs12',
+        'ssl.service_creds.pw'        => 'barBaz',
+        'ssl.trusted_certs'           => '/etc/xos/xtreemfs/truststore/osd.jks',
+        'ssl.trusted_certs.container' => 'jks',
+        'ssl.trusted_certs.pw'        => 'fooBar'
+      })
+    }
   end
 
   describe "should push credential file" do
-    should contain_file('/etc/xos/xtreemfs/truststore/osd.jks')
-    .with('source' => './osd.p12')
+    it {
+      should contain_file('/etc/xos/xtreemfs/truststore/osd.jks')
+      .with('source' => './osd.p12')
+    }
   end
 end
