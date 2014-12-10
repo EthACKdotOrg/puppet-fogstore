bef116fc9484817aa3c6066f8bae0efe5bae73d4
Cédric Jeanneret <tengu@ethack.org>
2014-12-10 06:48:06 +0100

    skeleton spec for role "osd"

--- spec/classes/role_osd_spec.rb
+++ spec/classes/role_osd_spec.rb
@@ -1,1 +1,32 @@
 require 'spec_helper'
+
+describe 'fogstore::roles::osd' do
+  let(:params) {{
+    :add_repo       => 'false',
+    :cred_format    => 'pkcs12',
+    :credential     => 'osd.pem',
+    :properties     => {},
+    :ssl_source_dir => '.',
+    :trusted_format => 'jks',
+    :trusted        => 'osd.jks',
+  }}
+
+  describe "should contain xtreemfs::role::storage" do
+    should contain_class('xtreemfs::role::storage')
+    .with('add_repo'                => 'false')
+    .with('dir_service'             => '')
+    .with('properties'              => {
+      'ssl.enable'                  => true,
+      'ssl.service_cred'            => '',
+      'ssl.service_cred.container'  => 'pkcs12',
+      'ssl.service_cred.pw'         => '',
+      'ssl.trusted_certs'           => '',
+      'ssl.trusted_certs.container' => 'jks',
+      'ssl.trusted_certs.pw'        => '',
+    })
+  end
+
+  describe "should push credential file" do
+    should contain_file('')
+  end
+end
