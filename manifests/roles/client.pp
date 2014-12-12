3e6c8049c8426b9f9468988b29dd256f0f21603f
Cédric Jeanneret <tengu@ethack.org>
2014-12-12 17:15:42 +0100

    skeleton for client

---
+++ manifests/roles/client.pp
@@ -0,0 +1,13 @@
+class fogstore::roles::client {
+  $credential      = $fogstore::cred_cert,
+  $add_repo        = $fogstore::add_repo,
+  $cred_format     = $fogstore::cred_format,
+  $cred_password   = $fogstore::cred_password,
+  $dir_host        = $fogstore::dir_host,
+  $dir_port        = $fogstore::dir_port,
+  $dir_protocol    = $fogstore::dir_protocol,
+  $mounts          = $fogstore::mounts,
+  $ssl_source_dir  = $fogstore::ssl_source_dir,
+  $trusted         = $fogstore::trusted,
+) {
+}
