f4ba3e7dd39e4e22ddc4e98476246f0d1219d0c7
Cédric Jeanneret <tengu@ethack.org>
2014-12-12 17:13:41 +0100

    prepare next tests

--- spec/fixtures/manifests/site.pp
+++ spec/fixtures/manifests/site.pp
@@ -1,5 +1,31 @@
 node default { }
 
+node client {
+  $repo = 'http://download.opensuse.org/repositories/home:/xtreemfs/xUbuntu_14.10/'
+  class {'::fogstore':
+    add_repo         => true,
+    apt_key_src      => "${repo}/Release.key",
+    cred_cert        => 'credential.pem',
+    cred_key         => 'credential.key',
+    cred_password    => 'credential-password',
+    dir_ca           => 'dir-ca.pem',
+    mrc_ca           => 'mrc-ca.pem',
+    osd_ca           => 'osd-ca.pem',
+    pkg_source       => $repo,
+    role             => 'client',
+    ssl_source_dir   => 'file://.',
+  }
+}
+
+node dir {
+}
+
+node introducer {
+}
+
+node mrc {
+}
+
 node osd {
   $repo = 'http://download.opensuse.org/repositories/home:/xtreemfs/xUbuntu_14.10/'
   class {'::fogstore':
@@ -11,7 +37,6 @@ node osd {
     cred_password    => 'credential-password',
     dir_ca           => 'dir-ca.pem',
     mrc_ca           => 'mrc-ca.pem',
-    osd_ca           => 'osd-ca.pem',
     osd_jks_password => 'osd-jks',
     pkg_source       => $repo,
     role             => 'osd',
@@ -19,14 +44,3 @@ node osd {
   }
 }
 
-node simple_client {
-}
-
-node simple_dir {
-}
-
-node simple_mrc {
-}
-
-node simple_introducer {
-}
