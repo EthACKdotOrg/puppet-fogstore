7017bcffbc0d9714df7b74d3f3c162615a23f8ed
Cédric Jeanneret <tengu@ethack.org>
2014-12-13 11:39:30 +0100

    new unit tests

---
+++ spec/classes/ssl_trusted_spec.rb
@@ -0,0 +1,181 @@
+require 'spec_helper'
+
+os_facts = @os_facts
+roles = @trusted_roles
+
+os_facts.each do |osfamily, facts|
+  describe 'fogstore::ssl::trusted' do
+
+    roles.each do |role|
+
+      let :facts do
+        facts
+      end
+
+      context "#{role} without password" do
+        let(:params) {{
+          :client_ca        => 'client-ca.pem',
+          :dir_ca           => 'dir-ca.pem',
+          :mrc_ca           => 'mrc-ca.pem',
+          :osd_ca           => 'osd-ca.pem',
+          :role             => role,
+          :ssl_source_dir   => 'file://.',
+        }}
+        it {
+          expect {
+            should compile.with_all_deps
+          }.to raise_error()
+        }
+      end
+
+      context "#{role} missing ca" do
+        let(:params) {{
+          :dir_ca           => 'dir-ca.pem',
+          :dir_jks_password => 'dir-jks-password',
+          :mrc_ca           => 'mrc-ca.pem',
+          :mrc_jks_password => 'mrc-jks-password',
+          :osd_ca           => 'osd-ca.pem',
+          :osd_jks_password => 'osd-jks-password',
+          :role             => role,
+          :ssl_source_dir   => 'file://.',
+        }}
+        it {
+          expect {
+            should compile.with_all_deps
+          }.to raise_error()
+        }
+      end
+
+    end
+
+    context "DIR: working" do
+      let(:params) {{
+        :client_ca        => 'client-ca.pem',
+        :dir_ca           => 'dir-ca.pem',
+        :dir_jks_password => 'dir-jks-password',
+        :mrc_ca           => 'mrc-ca.pem',
+        :osd_ca           => 'osd-ca.pem',
+        :role             => 'dir',
+        :ssl_source_dir   => 'file://.',
+      }}
+      it {
+        should compile.with_all_deps
+      }
+      it {
+        should contain_java_ks('dir_client_ca')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./client-ca.pem')
+        .with('target'      => '/etc/xos/xtreemfs/truststore/dir.jks')
+        .with('password'    => 'dir-jks-password')
+      }
+      it {
+        should contain_java_ks('dir_mrc_ca:/etc/xos/xtreemfs/truststore/dir.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./mrc-ca.pem')
+      }
+      it {
+        should contain_java_ks('dir_osd_ca:/etc/xos/xtreemfs/truststore/dir.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./osd-ca.pem')
+      }
+    end
+
+    context 'Introducer: working' do
+      let(:params) {{
+        :client_ca        => 'client-ca.pem',
+        :dir_ca           => 'dir-ca.pem',
+        :dir_jks_password => 'dir-jks-password',
+        :mrc_ca           => 'mrc-ca.pem',
+        :mrc_jks_password => 'mrc-jks-password',
+        :osd_ca           => 'osd-ca.pem',
+        :role             => 'introducer',
+        :ssl_source_dir   => 'file://.',
+      }}
+      it {
+        should compile.with_all_deps
+      }
+      it {
+        should contain_java_ks('dir_client_ca')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./client-ca.pem')
+        .with('target'      => '/etc/xos/xtreemfs/truststore/dir.jks')
+        .with('password'    => 'dir-jks-password')
+      }
+      it {
+        should contain_java_ks('dir_mrc_ca:/etc/xos/xtreemfs/truststore/dir.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./mrc-ca.pem')
+      }
+      it {
+        should contain_java_ks('dir_osd_ca:/etc/xos/xtreemfs/truststore/dir.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./osd-ca.pem')
+      }
+      it {
+        should contain_java_ks('mrc_client_ca')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./client-ca.pem')
+        .with('target'      => '/etc/xos/xtreemfs/truststore/mrc.jks')
+        .with('password'    => 'mrc-jks-password')
+      }
+      it {
+        should contain_java_ks('mrc_dir_ca:/etc/xos/xtreemfs/truststore/mrc.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./dir-ca.pem')
+        .with('password'    => 'mrc-jks-password')
+      }
+    end
+
+    context "MRC: working" do
+      let(:params) {{
+        :client_ca        => 'client-ca.pem',
+        :dir_ca           => 'dir-ca.pem',
+        :mrc_jks_password => 'mrc-jks-password',
+        :role             => 'mrc',
+        :ssl_source_dir   => 'file://.',
+      }}
+      it {
+        should compile.with_all_deps
+      }
+      it {
+        should contain_java_ks('mrc_client_ca')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./client-ca.pem')
+        .with('target'      => '/etc/xos/xtreemfs/truststore/mrc.jks')
+        .with('password'    => 'mrc-jks-password')
+      }
+      it {
+        should contain_java_ks('mrc_dir_ca:/etc/xos/xtreemfs/truststore/mrc.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./dir-ca.pem')
+        .with('password'    => 'mrc-jks-password')
+      }
+    end
+
+    context "OSD: working" do
+      let(:params) {{
+        :client_ca        => 'client-ca.pem',
+        :dir_ca           => 'dir-ca.pem',
+        :osd_jks_password => 'osd-jks-password',
+        :role             => 'osd',
+        :ssl_source_dir   => 'file://.',
+      }}
+      it {
+        should compile.with_all_deps
+      }
+      it {
+        should contain_java_ks('osd_client_ca')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./client-ca.pem')
+        .with('target'      => '/etc/xos/xtreemfs/truststore/osd.jks')
+        .with('password'    => 'osd-jks-password')
+      }
+      it {
+        should contain_java_ks('osd_dir_ca:/etc/xos/xtreemfs/truststore/osd.jks')
+        .with('ensure'      => 'latest')
+        .with('certificate' => 'file://./dir-ca.pem')
+        .with('password'    => 'osd-jks-password')
+      }
+    end
+  end
+end
