f6831040736432a900d333b2f1029ec6f1745241
Cédric Jeanneret <tengu@ethack.org>
2014-12-13 18:26:43 +0100

    added new role: dir

---
+++ manifests/roles/dir.pp
@@ -0,0 +1,66 @@
+# == Role: DIR
+#
+# Wrapper for xtreemfs::role::storage
+# Please refer to its documentation
+#
+# === Additional settings:
+#
+class fogstore::roles::dir(
+  $add_repo         = $fogstore::params::add_repo,
+  $client_ca        = $fogstore::params::client_ca,
+  $cred_format      = $fogstore::params::cred_format,
+  $cred_password    = $fogstore::params::cred_password,
+  $credential       = $fogstore::params::cred_cert,
+  $manage_jks       = $fogstore::params::manage_ssl,
+  $mrc_ca           = $fogstore::params::mrc_ca,
+  $osd_ca           = $fogstore::params::osd_ca,
+  $properties       = $fogstore::params::properties,
+  $ssl_source_dir   = $fogstore::params::ssl_source_dir,
+  $trusted          = $fogstore::params::trusted,
+  $trusted_format   = $fogstore::params::trusted_format,
+  $trusted_password = $fogstore::params::dir_jks_password,
+) inherits fogstore::params {
+
+  # Set SSL configuration by default
+  $local_properties = {
+    'ssl.enabled' =>
+      true,
+    'ssl.service_creds' =>
+      "${fogstore::params::cred_location}/${credential}",
+    'ssl.service_creds.container' =>
+      $cred_format,
+    'ssl.service_creds.pw' =>
+      $cred_password,
+    'ssl.trusted_certs' =>
+      "${fogstore::params::trust_location}/${trusted}",
+    'ssl.trusted_certs.container' =>
+      $trusted_format,
+    'ssl.trusted_certs.pw' =>
+      $trusted_password,
+  }
+
+  if ($manage_jks) {
+    class {'::fogstore::ssl::trusted':
+      client_ca        => $client_ca,
+      dir_jks_password => $trusted_password,
+      mrc_ca           => $mrc_ca,
+      osd_ca           => $osd_ca,
+      role             => 'dir',
+      ssl_source_dir   => $ssl_source_dir,
+    }
+  }
+
+  class {'::xtreemfs::role::directory':
+    add_repo   => $add_repo,
+    properties => merge($local_properties, $properties),
+  }
+
+  file {"${fogstore::params::cred_location}/${credential}":
+    ensure => file,
+    group  => 'xtreemfs',
+    mode   => '0640',
+    notify => Anchor[$::xtreemfs::internal::workflow::configure],
+    owner  => 'root',
+    source => "${ssl_source_dir}/${credential}",
+  }
+}
--- spec/classes/roles_dir_spec.rb
+++ spec/classes/roles_dir_spec.rb
@@ -1,1 +1,87 @@
 require 'spec_helper'
+
+os_facts = @os_facts
+
+describe 'fogstore::roles::dir' do
+  os_facts.each do |osfamily, facts|
+    let :facts do
+      facts
+    end
+
+    context 'Simple install' do
+      let(:pre_condition) do
+        "class {'::fogstore::roles::dir':
+          add_repo         => false,
+          client_ca        => 'client-ca.pem',
+          cred_password    => 'credential-password',
+          credential       => 'dir.p12',
+          mrc_ca           => 'mrc-ca.pem',
+          osd_ca           => 'osd-ca.pem',
+          properties       => {},
+          ssl_source_dir   => 'file://.',
+          trusted          => 'dir.jks',
+          trusted_password => 'dir-jks-password',
+        }"
+      end
+
+      it {
+        should compile.with_all_deps
+      }
+
+      it {
+        should contain_class('fogstore::ssl::trusted')
+      }
+
+      it {
+        should contain_file('/etc/ssl/certs/dir.p12')
+        .with('source' => 'file://./dir.p12')
+      }
+
+      it {
+        should contain_class('xtreemfs::role::directory')
+      }
+
+      it {
+        should contain_package('xtreemfs-server')
+      }
+    end
+
+    context 'Add properies' do
+      let(:pre_condition) do
+        "class {'::fogstore::roles::dir':
+          add_repo         => false,
+          client_ca        => 'client-ca.pem',
+          cred_password    => 'credential-password',
+          credential       => 'dir.p12',
+          mrc_ca           => 'mrc-ca.pem',
+          osd_ca           => 'osd-ca.pem',
+          properties       => {
+            discover       => true,
+            'listen.address' => '127.0.0.1',
+          },
+          ssl_source_dir   => 'file://.',
+          trusted          => 'dir.jks',
+          trusted_password => 'dir-jks-password',
+        }"
+      end
+
+      it {
+        should compile.with_all_deps
+      }
+      it {
+        should contain_class('xtreemfs::role::directory')
+        .with_properties({
+          'ssl.enabled'                 => true,
+          'ssl.service_creds'           => '/etc/ssl/certs/dir.p12',
+          'ssl.service_creds.container' => 'pkcs12',
+          'ssl.service_creds.pw'        => 'credential-password',
+          'ssl.trusted_certs'           => '/etc/xos/xtreemfs/truststore/dir.jks',
+          'ssl.trusted_certs.container' => 'jks',
+          'ssl.trusted_certs.pw'        => 'dir-jks-password',
+          'discover'                    => true,
+          'listen.address'              => '127.0.0.1',
+        })
+      }
+    end
+  end
+end
