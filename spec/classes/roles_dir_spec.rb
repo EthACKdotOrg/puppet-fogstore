a2ceee03fe65784a97db9e5b73fb64f2e2549fad
Cédric Jeanneret <tengu@ethack.org>
2014-12-18 06:41:04 +0100

    updated spec for role dir

--- spec/classes/roles_dir_spec.rb
+++ spec/classes/roles_dir_spec.rb
@@ -1,15 +1,86 @@
 require 'spec_helper'
 
+good_password = 'ua4Sua6IGhi0'
+
 describe 'fogstore::roles::dir' do
   on_supported_os.each do |os, facts|
     let :facts do
       facts
     end
 
+    context "missing admin_password on #{os}" do
+      let :pre_condition do
+        "
+        class {'fogstore::roles::dir':
+          add_repo         => false,
+          client_ca        => 'client-ca.pem',
+          cred_password    => 'credential-password',
+          credential       => 'dir.p12',
+          mrc_ca           => 'mrc-ca.pem',
+          osd_ca           => 'osd-ca.pem',
+          properties       => {},
+          ssl_source_dir   => 'file://.',
+          trusted          => 'dir.jks',
+          trusted_password => 'dir-jks-password',
+        }
+        "
+      end
+      it 'should fail' do
+        should raise_error(Puppet::Error, /validate_slength/)
+      end
+    end
+
+    context "short admin_password on #{os}" do
+      let :pre_condition do
+        "
+        class {'fogstore::roles::dir':
+          add_repo        => false,
+          admin_password  => 'foobarbaz',
+          client_ca        => 'client-ca.pem',
+          cred_password    => 'credential-password',
+          credential       => 'dir.p12',
+          mrc_ca           => 'mrc-ca.pem',
+          osd_ca           => 'osd-ca.pem',
+          properties       => {},
+          ssl_source_dir   => 'file://.',
+          trusted          => 'dir.jks',
+          trusted_password => 'dir-jks-password',
+        }
+        "
+      end
+      it 'should fail' do
+        should raise_error(Puppet::Error, /validate_slength/)
+      end
+    end
+
+    context "long admin_password on #{os}" do
+      let :pre_condition do
+        "
+        class {'fogstore::roles::dir':
+          add_repo        => false,
+          admin_password  => 'taeBafee8oov7eish6yiihakaexeu1eechuch6eighuam4Ib2ohxei0haifuTob2u',
+          client_ca        => 'client-ca.pem',
+          cred_password    => 'credential-password',
+          credential       => 'dir.p12',
+          mrc_ca           => 'mrc-ca.pem',
+          osd_ca           => 'osd-ca.pem',
+          properties       => {},
+          ssl_source_dir   => 'file://.',
+          trusted          => 'dir.jks',
+          trusted_password => 'dir-jks-password',
+        }
+        "
+      end
+      it 'should fail' do
+        should raise_error(Puppet::Error, /validate_slength/)
+      end
+    end
+
     context "Simple install on #{os}" do
       let(:pre_condition) do
         "class {'::fogstore::roles::dir':
           add_repo         => false,
+          admin_password   => '#{good_password}',
           client_ca        => 'client-ca.pem',
           cred_password    => 'credential-password',
           credential       => 'dir.p12',
@@ -54,6 +125,7 @@ describe 'fogstore::roles::dir' do
       let(:pre_condition) do
         "class {'::fogstore::roles::dir':
           add_repo         => false,
+          admin_password   => '#{good_password}',
           client_ca        => 'client-ca.pem',
           cred_password    => 'credential-password',
           credential       => 'dir.p12',
@@ -75,6 +147,7 @@ describe 'fogstore::roles::dir' do
       it {
         should contain_class('xtreemfs::role::directory')
         .with_properties({
+          'admin_password'              => good_password,
           'ssl.enabled'                 => true,
           'ssl.service_creds'           => '/etc/ssl/certs/dir.p12',
           'ssl.service_creds.container' => 'pkcs12',
