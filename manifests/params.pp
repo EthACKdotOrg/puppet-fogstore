702123ec677be17aff4d02329d5fcaebf201e371
Cédric Jeanneret <tengu@ethack.org>
2014-12-19 15:11:14 +0100

    added some more configurations in params, corrected default values

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,29 +4,29 @@ class fogstore::params (
   $admin_password   = '',
   $apt_key          = '07D6EA4F2FA7E736',
   $apt_key_name     = '/Release.key',
-  $client_ca        = false,
-  $cred_cert        = false,
+  $client_ca        = '',
+  $cred_cert        = '',
   $cred_certs       = {},
   $cred_format      = 'pkcs12',
-  $cred_key         = false,
+  $cred_key         = '',
   $cred_keys        = {},
   $cred_password    = '',
-  $dir_ca           = false,
+  $dir_ca           = '',
   $dir_jks_password = '',
-  $dir_host         = undef,
-  $dir_port         = undef,
-  $dir_protocol     = undef,
+  $dir_host         = 'localhost',
+  $dir_port         = '32636',
+  $dir_protocol     = 'pbrpcs',
   $manage_ssl       = true,
   $mounts           = {},
-  $mrc_ca           = false,
+  $mrc_ca           = '',
   $mrc_jks_password = '',
   $object_dir       = undef,
-  $osd_ca           = false,
+  $osd_ca           = '',
   $osd_jks_password = '',
   $properties       = {},
   $release          = '',
   $repos            = './',
-  $pkg_source       = false,
+  $pkg_source       = '',
   $ssl_source_dir   = '',
   $trusted_format   = 'jks',
   $trusted          = 'trusted.jks',
@@ -36,6 +36,9 @@ class fogstore::params (
   $apt_key_src = "${pkg_source}/${apt_key_name}"
 
   $cred_location = '/etc/ssl/certs'
+
+  $min_pwd_length = 12
+  $max_pwd_length = 64
 
   # where we put the credentials and trust stores
   $trust_location = '/etc/xos/xtreemfs/truststore'
